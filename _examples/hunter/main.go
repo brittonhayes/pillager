6ef957006b183fb478dff9661756fa20481a98dd
brittonhayes <brittonhayes@users.noreply.github.com>
2022-03-01 23:02:03 -0800

    fix: repair hunter example

--- _examples/hunter/main.go
+++ _examples/hunter/main.go
@@ -3,40 +3,33 @@ package main
 import (
 	"os"
 
-	"github.com/brittonhayes/pillager"
+	"github.com/brittonhayes/pillager/pkg/format"
 	"github.com/brittonhayes/pillager/pkg/hunter"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	err := example()
-	if err != nil {
-		panic(err)
-	}
-}
-
-func example() error {
-	opts := []pillager.ConfigOption{
-		pillager.WithLogLevel(zerolog.DebugLevel),
-	}
-
 	// Create a new hunter config
-	p, err := hunter.New(opts...)
+	h, err := hunter.New(
+		hunter.WithScanPath("."),
+		hunter.WithWorkers(2),
+		hunter.WithFormat(format.Simple{}),
+		hunter.WithLogLevel(zerolog.DebugLevel.String()),
+	)
 	if err != nil {
-		return err
+		log.Fatal().Err(err).Send()
 	}
 
 	// Start hunting
-	results, err := p.Hunt()
+	results, err := h.Hunt()
 	if err != nil {
-		return err
+		log.Fatal().Err(err).Send()
 	}
 
 	// Report results
-	err = p.Report(os.Stdout, results)
+	err = h.Report(os.Stdout, results)
 	if err != nil {
-		return err
+		log.Fatal().Err(err).Send()
 	}
-
-	return nil
 }
